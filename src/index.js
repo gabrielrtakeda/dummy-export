796b2cc94153d7a685d6d9764fb944d5c38ef196
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-03-06 16:33:02 -0300

    feat(package): add babel to compile source to lib

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import WebFont from 'webfontloader'
 
-import * as assets from './assets'
-import * as wait from './await'
-import * as cards from './cards'
-import * as content from './content'
-import * as dummy from './dummy'
-import * as form from './form'
-import * as layout from './layout'
-import * as list from './list'
-import * as navigation from './navigation'
-import * as progress from './progress'
-
 WebFont.load({
   google: {
     families: ['Nunito Sans:400,600,800', 'sans-serif', 'Source Sans Pro:400,700']
   }
 })
 
-export default {
-  assets,
-  await: wait,
-  cards,
-  content,
-  dummy,
-  form,
-  layout,
-  list,
-  navigation,
-  progress
-}
+export * from './assets'
+export * from './await'
+export * from './cards'
+export * from './content'
+export * from './dummy'
+export * from './form'
+export * from './layout'
+export * from './list'
+export * from './navigation'
+export * from './progress'
